8e4f74325ec7f1f87c4d5b865f5e070f16f89e85
edgarduarteito <edgar.duarte@ito-software.com>
2015-01-22 09:17:21 -0500

    Cambio Services 22 enero 2015 9:16
    se implemento servicio para el manejo de listado de animes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,22 +1,20 @@
-(function (){
-    angular.module('anime.controllers',[])
-      .controller('PokedexController', ['$scope', 'AnimeService', function ($scope, $AnimeService) {
-      $scope.pokemons = [];
+(function () {
+    angular.module('anime.controllers', [])
+            .controller('PokedexController', ['$scope', 'AnimeService', function ($scope, AnimeService) {
+                    AnimeService.all().then(function (data) {
+                        $scope.pokemons = data;
+                    })
 
-      $http.get('animes.json')
-        .success(function (data) {
-          $scope.pokemons = data;
-        });
-    }])
-            .controller('AnimeController',['$scope', function ($scope) {
-        $scope.anime = {
-            id:'01',
-            titulo: 'NARUTO SHIPPUDEN',
-            imagen:'naruto',
-            tipo: 'Serie',
-            genero: ['Super Poderes', 'Shounen', 'Artes Marciales', 'Comedia', 'Accion'],
-            estado: 'En emision',
-            descripcion: 'Pasan dos años después de que Naruto y Sakura se fueran \n\
+                }])
+            .controller('AnimeController', ['$scope', function ($scope) {
+                    $scope.anime = {
+                        id: '01',
+                        titulo: 'NARUTO SHIPPUDEN',
+                        imagen: 'naruto',
+                        tipo: 'Serie',
+                        genero: ['Super Poderes', 'Shounen', 'Artes Marciales', 'Comedia', 'Accion'],
+                        estado: 'En emision',
+                        descripcion: 'Pasan dos años después de que Naruto y Sakura se fueran \n\
 a entrenar cada uno con su maestro sannin, en este caso, Naruto se fue con Jiraya y Sakura con Tsunade. \n\
 Ya tienen 14 años y son unos geniales ninjas, \n\
 Naruto ha aprendido muchas técnicas nuevas y ha perfeccionado el Kage Bunshin \n\
@@ -27,20 +25,20 @@ Sakura ha aprendido todo sobre la medicina ninja, y se ha convertido en una ninj
  los Akatsuki se han preparado para lo que viene. \n\
 Empiezan a mover ficha yendo a buscar a los 9 demonios de chakra legendario \n\
 (Kyubi, Shukaku de la arena y siete más).',
-            episodios: ['1', '2', '3', '4', '5', '6', '390', '391']
+                        episodios: ['1', '2', '3', '4', '5', '6', '390', '391']
 
 
-        };
+                    };
 
-    }])
-    .controller('TabsController', function () {
-        this.tab = 1;
-        this.selectTab = function (tab) {
-            this.tab = tab;
+                }])
+            .controller('TabsController', function () {
+                this.tab = 1;
+                this.selectTab = function (tab) {
+                    this.tab = tab;
 
-        };
+                };
 
-    });
-    
+            });
+
 })();
 
